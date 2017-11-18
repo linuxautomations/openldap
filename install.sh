b9edd8533a559e41944914d8e0b872a08446280d
linuxautomations <raghukx@gmail.com>
2017-11-18 15:56:59 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,8 @@ systemctl start slapd
 systemctl enable slapd 
 
 cd /opt
-git clone https://github.com/indexit-devops/ldap.git &>/dev/null
+git clone https://github.com/linuxautomations/openldap.git &>/dev/null
+[ $? -eq 0 ] && echo "Failed with pulling repo" && exit 1
 cd /opt/ldap
 ldapadd -Y EXTERNAL -H ldapi:/// -f chrootpw.ldif 
 ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/cosine.ldif 
